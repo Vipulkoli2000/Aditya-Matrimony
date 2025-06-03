4d1a725f05ec917c6e12f650ec3ffdc96cf1b247
Vipulkoli2000 <Vipulkoli2323@gmail.com>
2025-06-03 12:18:19 +0530

    gitignore fix

--- config/mail.php
+++ config/mail.php
@@ -104,7 +104,7 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'noreply@marathavivahmandaldombivli.com'),
-        'name' => env('MAIL_FROM_NAME', 'Matrimony via Brevo'),
+        'name' => env('MAIL_FROM_NAME', 'Matrimony'),
     ],
 
     /*
